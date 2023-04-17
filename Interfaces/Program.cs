ceebe1cd604801b447c7766786bde9b965b74f62
leticialbuk <leticia.albuk@gmail.com>
2023-04-17 17:04:36 -0300

    Interfaces e Classes Abstratas

---
+++ Interfaces/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public abstract class Pagamento : IPagamento
+    {
+        public DateTime Vencimento { get; set; }
+
+        public virtual void Pagar(double valor)
+        {
+            // aqui voce pode executar algo base como uma validação
+        }
+    }
+
+    public class PagamentoCartaoCredito : Pagamento
+    {
+        public override void Pagar(double valor)
+        {
+            base.Pagar(valor);
+        }
+    }
+    public class PagamentoBoleto : Pagamento
+    {
+        public override void Pagar(double valor)
+        {
+            base.Pagar(valor);
+        }
+    }
+
+    // Interface dita como uma classe deve ser, I sempre na frente das interfaces
+    public interface IPagamento
+    {
+        DateTime Vencimento { get; set; }
+
+        void Pagar(double valor);
+    }
+}
