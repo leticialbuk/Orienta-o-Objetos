20ce2fb78788c50ed80d1b35be2676228eb417e8
leticialbuk <leticia.albuk@gmail.com>
2023-04-18 16:36:36 -0300

    Aprendendo sobre listas

---
+++ List/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var payments = new List<Payment>();
+            payments.Add(new Payment(1));
+            payments.Add(new Payment(2));
+            payments.Add(new Payment(3));
+            payments.Add(new Payment(4));
+            payments.Add(new Payment(5));
+
+            foreach (var item in payments)
+            {
+                Console.WriteLine(item.Id);
+            }
+
+            //First analisa se existe o numero 3 na lista
+            var payment = payments.First(x => x.Id == 3);
+            Console.WriteLine(payment.Id);
+        }
+    }
+
+    public class Payment
+    {
+        public int Id { get; set; }
+
+        public Payment(int id)
+        {
+            Id = id;
+        }
+    }
+}
