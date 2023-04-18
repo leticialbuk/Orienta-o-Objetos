07a493b7eb76ebae7b9d0b50ccfecf23af9acf80
leticialbuk <leticia.albuk@gmail.com>
2023-04-17 22:31:21 -0300

    Utilizando delegate para apontar outra funcao

---
+++ Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void RealizarPagamento(double valor)
+        {
+            Console.WriteLine($"Pago o valor de R$ {valor} reais");
+        }
+
+        static void Main(string[] args)
+        {
+            var pagar = new Pagamento.Pagar(RealizarPagamento);
+            pagar(25);
+        }
+    }
+
+    // Delegate é usado para apomtar/chamar outra função
+    public class Pagamento
+    {
+        public delegate void Pagar(double valor);
+
+    }
+}
