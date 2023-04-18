ffda8c3b81e19072be8376265a75d0c92f8181db
leticialbuk <leticia.albuk@gmail.com>
2023-04-17 23:30:13 -0300

    Utilizando Events para notificar outras classes/obj caso algo ocorra

---
+++ Events/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            var room = new Room(3);
+            room.RoomSoldOutEvent += OnRoomSoldOut;
+
+            room.ReserveSeat();
+            room.ReserveSeat();
+            room.ReserveSeat();
+        }
+
+        static void OnRoomSoldOut(object sender, EventArgs e)
+        {
+            Console.WriteLine("Sala cheia!");
+        }
+    }
+
+    public class Room
+    {
+        public Room(int seats)
+        {
+            Seats = seats;
+            seatsInUse = 0;
+        }
+
+        private int seatsInUse = 0;
+        public int Seats { get; set; }
+
+        public void ReserveSeat()
+        {
+            seatsInUse++;
+            if (seatsInUse >= Seats)
+            {
+                OnRoomSoldOut(EventArgs.Empty);
+            }
+            else
+            {
+                Console.WriteLine("Assento reservado");
+            }
+        }
+
+        public event EventHandler RoomSoldOutEvent;
+
+        protected virtual void OnRoomSoldOut(EventArgs e)
+        {
+            EventHandler handler = RoomSoldOutEvent;
+            handler?.Invoke(this, e);
+        }
+    }
+}
